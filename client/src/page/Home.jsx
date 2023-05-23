bc19cd808eba1545fe496b9ae6e8eb7b1ed8c0d1
AlexWang233 <alex13901350@gmail.com>
2023-05-23 01:40:24 -0400

    Demo

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { PageHOC } from "../components";
+import { useGlobalContext } from "../context";
 
 const Home = () => {
+  const { demo } = useGlobalContext();
   return (
     <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
-      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
+      <h1 className="text-white text-xl">{demo}</h1>
     </div>
   );
 };
 
-export default Home;
+export default PageHOC(
+  Home,
+  <>
+    Welcome to Avax Gods <br />A Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br />
+    The ultimate Web3 Battle Card Game
+  </>
+);
